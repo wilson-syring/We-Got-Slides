17906e35e5658c277d73fe3384b5c0da297b8377
Wilson Syring <wilson.syring@gmail.com>
2023-03-16 13:12:17 -0500

    completed the scroll in guided exercise with my own flair added to the style and content.

--- js/external.js
+++ js/external.js
@@ -1,8 +1,9 @@
+//this function is for controlling the scroll event, so it saves on performance
 function debounce(func, wait = 20, immediate = true) {
     let timeout;
-    return function() {
+    return function () {
         let context = this, args = arguments;
-        let later = function() {
+        let later = function () {
             timeout = null;
             if (!immediate) func.apply(context, args);
         };
@@ -12,8 +13,29 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 };
- const sliderImages= document.querySelectorAll('.slide-in');
- function checkSlide(e){
 
- }
- window.addEventListener('scroll',debounce(checkSlide));
+//variable for grabbing the image elements
+const sliderImages = document.querySelectorAll('.slide-in');
+
+//function for checking where you have scrolled to on the page and sliding images in and out.
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+        //sets a breakpoint to halfway through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        //sets another breakpoint to the bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        //variable for determining if image is half  way shown
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        //variable for determining if image is not scrolled past yet
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        //if statement for toggling the active class to the image element
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
+}
+
+//event listener for scrolling the page
+window.addEventListener('scroll', debounce(checkSlide));
